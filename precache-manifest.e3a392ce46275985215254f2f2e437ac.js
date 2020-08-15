408ba91c40155e03fe67de9140b4c830e2e1b61b
Garvit291 <chhabragarvit291@outlook.com>
2020-08-15 23:36:30 +0530

    Updates

---
+++ precache-manifest.e3a392ce46275985215254f2f2e437ac.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac733eba52c38cd9c80075af2fcecfa",
+    "url": "/project-landing-page/index.html"
+  },
+  {
+    "revision": "6680d475713f2ed8545a",
+    "url": "/project-landing-page/static/css/main.cff3704f.chunk.css"
+  },
+  {
+    "revision": "249e96ec2fb73a6694bc",
+    "url": "/project-landing-page/static/js/2.a75be3f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-landing-page/static/js/2.a75be3f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6680d475713f2ed8545a",
+    "url": "/project-landing-page/static/js/main.ab38684a.chunk.js"
+  },
+  {
+    "revision": "f9cecce612415e6a7f0b",
+    "url": "/project-landing-page/static/js/runtime-main.fe71644e.js"
+  },
+  {
+    "revision": "314aee4f778fc19042c1e2be65fa4dfa",
+    "url": "/project-landing-page/static/media/1.314aee4f.jpg"
+  },
+  {
+    "revision": "134a983e1d1f92f01010414afff262fb",
+    "url": "/project-landing-page/static/media/10.134a983e.jpg"
+  },
+  {
+    "revision": "04eb87209041ab6a9dbdd32db4f30457",
+    "url": "/project-landing-page/static/media/11.04eb8720.jpg"
+  },
+  {
+    "revision": "c8a03b11d0009b2cfddd4a4ee4938782",
+    "url": "/project-landing-page/static/media/2.c8a03b11.jpg"
+  },
+  {
+    "revision": "a26a54678bead0d92cc533bd1229397b",
+    "url": "/project-landing-page/static/media/3.a26a5467.jpg"
+  },
+  {
+    "revision": "c94fcfd47f491b697c40d048c3b10889",
+    "url": "/project-landing-page/static/media/4.c94fcfd4.jpg"
+  },
+  {
+    "revision": "f747c75f43fc6d456465576ba6d5d6bc",
+    "url": "/project-landing-page/static/media/5.f747c75f.jpg"
+  },
+  {
+    "revision": "0f5244513154ba956c3548aa8e89ceb9",
+    "url": "/project-landing-page/static/media/6.0f524451.jpg"
+  },
+  {
+    "revision": "c36d5ce5cf5a333ade5474731791ddbd",
+    "url": "/project-landing-page/static/media/7.c36d5ce5.jpg"
+  },
+  {
+    "revision": "7ee630da65b011a939a3733972d5a0da",
+    "url": "/project-landing-page/static/media/8.7ee630da.jpg"
+  },
+  {
+    "revision": "dad13688bd12acd7d2e49d0dadfb121e",
+    "url": "/project-landing-page/static/media/9.dad13688.jpg"
+  }
+]);
